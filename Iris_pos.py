df48bb53649014c3fd58711498a10e78f1c02172
joshuamd21 <112509156+joshuamd21-school@users.noreply.github.com>
2023-04-16 20:31:41 -0500

    refactored code
    Refactored the code to now hold most of the constant information in an eye tracking class and refactored many other values also to be held in the class as well.

--- Iris_pos.py
+++ Iris_pos.py
@@ -8,10 +8,20 @@ import time
 
 class eye_tracking():
     def __init__(self):
-        self.CALIBRATION_STEPS = 30
+        self.camera = cv.VideoCapture(0)  # setting up of camera
+
+        self.face_mesh = mp_face_mesh.FaceMesh(
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )  # face mesh used for eye and iris detection
+
+        self.CALIBRATION_STEPS = 30  # amount of data collected for calibration
 
         self.vert_slope = -0.15
         self.vert_intercept = 3.33
+
         self.horiz_slope = 0.16
         self.horiz_intercept = 3.19
 
@@ -23,6 +33,22 @@ class eye_tracking():
         self.h_iter = 0
         self.horizontal_ratios = np.zeros(self.H_ITER_MAX)
         self.screen_width, self.screen_height = mouse.size()
+
+        self.LEFT_EYE = [362, 382, 381, 380, 374, 373, 390,
+                         249, 263, 466, 388, 387, 386, 385, 384, 398]
+        self.RIGHT_EYE = [33, 7, 163, 144, 145, 154,
+                          133, 173, 157, 158, 159, 160, 161, 246]
+
+        self.RIGHT_IRIS = [474, 475, 476, 477]
+        self.LEFT_IRIS = [469, 470, 471, 472]
+
+        self.L_LEFT = [33]
+        self.L_RIGHT = [133]
+
+        self.R_LEFT = [362]  # right eye left point
+        self.R_TOP = [257]  # right eye top point
+        self.R_BOTTOM = [253]  # right eye bottom point
+        self.R_RIGHT = [263]  # right eye right point
 
     def iris_ratio(self, iris_center, point1, point2):
         center_to_point1_dist = euclidean_distance(iris_center, point1)
@@ -83,77 +109,55 @@ def euclidean_distance(point1, point2):
 if __name__ == "__main__":
     mp_face_mesh = mp.solutions.face_mesh
 
-    LEFT_EYE = [362, 382, 381, 380, 374, 373, 390,
-                249, 263, 466, 388, 387, 386, 385, 384, 398]
-    RIGHT_EYE = [33, 7, 163, 144, 145, 154,
-                 133, 173, 157, 158, 159, 160, 161, 246]
-
-    RIGHT_IRIS = [474, 475, 476, 477]
-    LEFT_IRIS = [469, 470, 471, 472]
-
-    L_H_LEFT = [33]
-    L_H_RIGHT = [133]
-    R_LEFT = [362]  # right eye point
-    R_TOP = [257]  # right eye top point
-    R_BOTTOM = [253]  # right eye bottom point
-    R_RIGHT = [263]  # right eye right point
     mouse.FAILSAFE = False
     tracking = eye_tracking()
+    while True:
+        ret, frame = tracking.camera.read()
+        if not ret:
+            break
 
-    cap = cv.VideoCapture(0)
-    with mp_face_mesh.FaceMesh(
-        max_num_faces=1,
-        refine_landmarks=True,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5
-    ) as face_mesh:
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
+        frame = cv.flip(frame, 1)
 
-            frame = cv.flip(frame, 1)
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        results = tracking.face_mesh.process(rgb_frame)
+        img_h, img_w = frame.shape[:2]
+        if results.multi_face_landmarks:
+            mesh_points = np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(
+                int) for p in results.multi_face_landmarks[0].landmark])
+            (l_cx, l_cy), l_radius = cv.minEnclosingCircle(
+                mesh_points[tracking.LEFT_IRIS])
+            (r_cx, r_cy), r_radius = cv.minEnclosingCircle(
+                mesh_points[tracking.RIGHT_IRIS])
+            center_left = np.array([l_cx, l_cy], dtype=np.int32)
+            center_right = np.array([r_cx, r_cy], dtype=np.int32)
+            cv.circle(frame, center_left, int(l_radius),
+                      (255, 0, 255), 1, cv.LINE_AA)
+            cv.circle(frame, center_right, int(r_radius),
+                      (255, 0, 255), 1, cv.LINE_AA)
+            cv.circle(frame, mesh_points[tracking.R_RIGHT][0], 2,
+                      (255, 255, 255), 1, cv.LINE_AA)
+            cv.circle(frame, mesh_points[tracking.R_LEFT][0], 2,
+                      (0, 255, 255), 1, cv.LINE_AA)
+            cv.circle(frame, mesh_points[tracking.R_BOTTOM][0], 2,
+                      (255, 255, 255), 1, cv.LINE_AA)
+            cv.circle(frame, mesh_points[tracking.R_TOP][0], 2,
+                      (0, 255, 255), 1, cv.LINE_AA)
+            horiz_ratio = tracking.iris_ratio(
+                center_right, mesh_points[tracking.R_RIGHT], mesh_points[tracking.R_LEFT][0])
+            vert_ratio = tracking.iris_ratio(
+                center_right, mesh_points[tracking.R_TOP], mesh_points[tracking.R_BOTTOM][0])
+            cv.putText(
+                frame, f"Horizontal ratio: {horiz_ratio: 0.2f}", (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+            cv.putText(
+                frame, f"Vertical ratio: {vert_ratio: 0.2f}", (30, 90), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
 
-            rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-            results = face_mesh.process(rgb_frame)
-            img_h, img_w = frame.shape[:2]
-            if results.multi_face_landmarks:
-                mesh_points = np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(
-                    int) for p in results.multi_face_landmarks[0].landmark])
-                (l_cx, l_cy), l_radius = cv.minEnclosingCircle(
-                    mesh_points[LEFT_IRIS])
-                (r_cx, r_cy), r_radius = cv.minEnclosingCircle(
-                    mesh_points[RIGHT_IRIS])
-                center_left = np.array([l_cx, l_cy], dtype=np.int32)
-                center_right = np.array([r_cx, r_cy], dtype=np.int32)
-                cv.circle(frame, center_left, int(l_radius),
-                          (255, 0, 255), 1, cv.LINE_AA)
-                cv.circle(frame, center_right, int(r_radius),
-                          (255, 0, 255), 1, cv.LINE_AA)
-                cv.circle(frame, mesh_points[R_RIGHT][0], 2,
-                          (255, 255, 255), 1, cv.LINE_AA)
-                cv.circle(frame, mesh_points[R_LEFT][0], 2,
-                          (0, 255, 255), 1, cv.LINE_AA)
-                cv.circle(frame, mesh_points[R_BOTTOM][0], 2,
-                          (255, 255, 255), 1, cv.LINE_AA)
-                cv.circle(frame, mesh_points[R_TOP][0], 2,
-                          (0, 255, 255), 1, cv.LINE_AA)
-                horiz_ratio = tracking.iris_ratio(
-                    center_right, mesh_points[R_RIGHT], mesh_points[R_LEFT][0])
-                vert_ratio = tracking.iris_ratio(
-                    center_right, mesh_points[R_TOP], mesh_points[R_BOTTOM][0])
-                cv.putText(
-                    frame, f"Horizontal ratio: {horiz_ratio: 0.2f}", (30, 30), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
-                cv.putText(
-                    frame, f"Vertical ratio: {vert_ratio: 0.2f}", (30, 90), cv.FONT_HERSHEY_PLAIN, 1.2, (0, 255, 0), 1, cv.LINE_AA)
+            tracking.move_mouse(horiz_ratio, 0.4)
+        cv.imshow('img', frame)
 
-                tracking.move_mouse(horiz_ratio, 0.5)
-            cv.imshow('img', frame)
-
-            # Exit the loop if the 'q' key is pressed
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
+        # Exit the loop if the 'q' key is pressed
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
     # Release the webcam and close the window
-    cap.release()
+    tracking.camera.release()
     cv.destroyAllWindows()
